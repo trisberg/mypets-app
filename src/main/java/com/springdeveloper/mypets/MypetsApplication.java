6eaee21af132c7ec2cf154ba2a5b08a94e4b91c1
trisberg <trisberg@pivotal.io>
2019-07-19 10:06:36 -0400

    Add dekorateio/dekorate annotations and Dockerfile

--- src/main/java/com/springdeveloper/mypets/MypetsApplication.java
+++ src/main/java/com/springdeveloper/mypets/MypetsApplication.java
@@ -3,6 +3,23 @@ package com.springdeveloper.mypets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.dekorate.kubernetes.annotation.Env;
+import io.dekorate.kubernetes.annotation.ImagePullPolicy;
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
+import io.dekorate.kubernetes.annotation.Port;
+import io.dekorate.kubernetes.annotation.ServiceType;
+import io.dekorate.servicecatalog.annotation.Parameter;
+import io.dekorate.servicecatalog.annotation.ServiceCatalog;
+import io.dekorate.servicecatalog.annotation.ServiceCatalogInstance;
+
+@KubernetesApplication(serviceType = ServiceType.LoadBalancer,
+	ports = @Port(name = "web", containerPort = 8080), 
+	envVars = {@Env(name = "SPRING_PROFILES_ACTIVE", value = "kubernetes")}, 
+	imagePullPolicy = ImagePullPolicy.Always)
+@ServiceCatalog(instances = @ServiceCatalogInstance(name = "mypets", 
+		serviceClass = "mysql", servicePlan = "5-7-14", 
+		bindingSecret = "mypets-credentials",
+		parameters = {@Parameter(key = "mysqlDatabase", value = "mypets")}))
 @SpringBootApplication
 public class MypetsApplication {
 
