33f723cef3c5abe2b7d0bcdfe74152f8db3bc8fc
trisberg <trisberg@pivotal.io>
2019-07-24 18:13:46 -0400

    Add service binding annotation

--- src/main/java/com/springdeveloper/mypets/MypetsApplication.java
+++ src/main/java/com/springdeveloper/mypets/MypetsApplication.java
@@ -3,23 +3,18 @@ package com.springdeveloper.mypets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.dekorate.kubernetes.annotation.Annotation;
 import io.dekorate.kubernetes.annotation.Env;
 import io.dekorate.kubernetes.annotation.ImagePullPolicy;
 import io.dekorate.kubernetes.annotation.KubernetesApplication;
 import io.dekorate.kubernetes.annotation.Port;
 import io.dekorate.kubernetes.annotation.ServiceType;
-import io.dekorate.servicecatalog.annotation.Parameter;
-import io.dekorate.servicecatalog.annotation.ServiceCatalog;
-import io.dekorate.servicecatalog.annotation.ServiceCatalogInstance;
 
 @KubernetesApplication(serviceType = ServiceType.LoadBalancer,
-	ports = @Port(name = "web", containerPort = 8080), 
+	ports = @Port(name = "http", containerPort = 80),
+	annotations = {@Annotation(key = "service.projectriff.io/binding-secrets", value = "mypets-credentials")},
 	envVars = {@Env(name = "SPRING_PROFILES_ACTIVE", value = "kubernetes")}, 
 	imagePullPolicy = ImagePullPolicy.Always)
-@ServiceCatalog(instances = @ServiceCatalogInstance(name = "mypets", 
-		serviceClass = "mysql", servicePlan = "5-7-14", 
-		bindingSecret = "mypets-credentials",
-		parameters = {@Parameter(key = "mysqlDatabase", value = "mypets")}))
 @SpringBootApplication
 public class MypetsApplication {
 
