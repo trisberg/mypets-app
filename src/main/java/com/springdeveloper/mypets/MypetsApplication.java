6b1fc6efe15ed1617d21e681a73c2bbfa532458a
trisberg <trisberg@pivotal.io>
2019-07-24 20:56:50 -0400

    Switch to dekorate properties instead of annotations

--- src/main/java/com/springdeveloper/mypets/MypetsApplication.java
+++ src/main/java/com/springdeveloper/mypets/MypetsApplication.java
@@ -3,18 +3,6 @@ package com.springdeveloper.mypets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import io.dekorate.kubernetes.annotation.Annotation;
-import io.dekorate.kubernetes.annotation.Env;
-import io.dekorate.kubernetes.annotation.ImagePullPolicy;
-import io.dekorate.kubernetes.annotation.KubernetesApplication;
-import io.dekorate.kubernetes.annotation.Port;
-import io.dekorate.kubernetes.annotation.ServiceType;
-
-@KubernetesApplication(serviceType = ServiceType.LoadBalancer,
-	ports = @Port(name = "http", containerPort = 80),
-	annotations = {@Annotation(key = "service.projectriff.io/binding-secrets", value = "mypets-credentials")},
-	envVars = {@Env(name = "SPRING_PROFILES_ACTIVE", value = "kubernetes")}, 
-	imagePullPolicy = ImagePullPolicy.Always)
 @SpringBootApplication
 public class MypetsApplication {
 
