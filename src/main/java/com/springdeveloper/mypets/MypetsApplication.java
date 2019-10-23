bac6d6fc49c3fc9fc3cafb1ae035d69de32b7c54
trisberg <trisberg@pivotal.io>
2019-10-23 10:25:14 -0400

    add reload strategy

--- src/main/java/com/springdeveloper/mypets/MypetsApplication.java
+++ src/main/java/com/springdeveloper/mypets/MypetsApplication.java
@@ -1,10 +1,19 @@
 package com.springdeveloper.mypets;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+@RefreshScope
 @SpringBootApplication
 public class MypetsApplication {
+
+	@Value("${spring.data.mongodb.database:}")
+	String database;
+
+	// @Autowired
+    // private RestartEndpoint restartEndpoint;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MypetsApplication.class, args);
